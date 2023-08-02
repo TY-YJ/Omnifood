cf74a5fb88a0afaf6e62ed2077e25a574c3cd712
tyyj0430 <kim.taeyo@northeastern.edu>
2023-08-02 13:54:23 -0700

    omnifood

---
+++ js/redirect.js
@@ -0,0 +1,13 @@
+
+
+
+// document.getElementById("sign-up-btn").addEventListener("click", redirectPage);
+function redirectFunc() {
+
+    window.location.href = 'http://127.0.0.1:5500/confirmation.html';
+}
+
+function redirectBack() {
+
+    window.location.replace('http://127.0.0.1:5500/index.html');
+}
